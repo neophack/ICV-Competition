d466cf36b64931f06d1a274d1299b125c931d2f6
micuks <sdkjwql@gmail.com>
2021-08-30 17:41:10 +0800

    added clean.sh

--- release.sh
+++ release.sh
@@ -2,9 +2,7 @@ cd ./build/
 sh build_release.sh
 cd ../
 rm Controller.zip
-zip -j Controller.zip ./bin/AEB
-zip -j Controller.zip ./bin/AVP
-zip -j Controller.zip ./lib/*
+zip -j Controller.zip ./bin/*
 echo -e "\a"
 echo Success!!!
 date
